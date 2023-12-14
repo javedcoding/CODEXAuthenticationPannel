854f7ad6593bebc63f1a94e8044fed975ee0bb14
Hungbth2000 <hung.buitranhai@stud.fra-uas.de>
2023-12-14 23:55:15 +0100

    update changes

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 import re
 
 from django import forms
-# from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from .models import UserProfile
--- app/models.py
+++ app/models.py
@@ -36,14 +36,14 @@ class UserProfile(AbstractUser):
     def __str__(self) -> str:
         return f"{self.username} Profile"
 
-    # def save(self, *args, **kwargs) -> None:
+    def save(self, *args, **kwargs) -> None:
 
-    #     # Call the parent class's save method
-    #     super().save(*args, **kwargs)
+        # Call the parent class's save method
+        super().save(*args, **kwargs)
 
-    #     img = Image.open(self.image.path)
+        img = Image.open(self.image.path)
 
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
